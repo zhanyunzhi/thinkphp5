d07f769c1d14cf550ba53a9cc8f8bff20bf8baba
zhanyunzhi <540562332@qq.com>
2017-06-30 01:05:51 +0800

    生成激活码，并输出文件

--- application/common.php
+++ application/common.php
@@ -26,3 +26,32 @@ function return_result($info = '', $code = '0000', $data = array()) {
     echo json_encode($out, JSON_HEX_TAG);
     exit(0);
 }
+    /**
+     * 比较标准的接口输出函数
+     * @param string  $namespace 命名空间
+     * @return string  32位唯一标识码
+     */
+function create_guid($namespace = '') {
+    static $guid = '';
+    $uid = uniqid("", true);
+    $data = $namespace;
+    $data .= $_SERVER['REQUEST_TIME'];
+    $data .= $_SERVER['HTTP_USER_AGENT'];
+    $data .= $_SERVER['SERVER_ADDR'];
+    $data .= $_SERVER['SERVER_PORT'];
+    $data .= $_SERVER['REMOTE_ADDR'];
+    $data .= $_SERVER['REMOTE_PORT'];
+    $hash = strtoupper(hash('ripemd128', $uid . $guid . md5($data)));
+    $guid = '' .
+        substr($hash, 0, 8) .
+        '-' .
+        substr($hash, 8, 4) .
+        '-' .
+        substr($hash, 12, 4) .
+        '-' .
+        substr($hash, 16, 4) .
+        '-' .
+        substr($hash, 20, 12) .
+        '';
+    return $guid;
+}
--- application/index/controller/active.php
+++ application/index/controller/active.php
@@ -45,6 +45,11 @@ class Active {
                 ->where("is_sale","eq",1)
                 ->find();
             if($data){
+                Db::table("active_num")                 //访问次数加一
+                    ->where("device_id","eq",$deviceNum)
+                    ->where("is_use","eq",1)
+                    ->where("is_sale","eq",1)
+                    ->setInc('visit_amount');
                 $returnData = array('msg'=>'已激活');
                 return_result('success', '0000', $returnData);
             }else{
@@ -53,6 +58,48 @@ class Active {
             }
         }catch(\Exception $e){
             $returnData = array('msg'=>'服务器暂时无法验证您的信息');
+            return_result('error', '2000', $returnData);
+        }
+    }
+    //批量生成激活码
+    public function create_active_num() {
+        $count = empty($_GET['count']) ? 100 : $_GET['count'];          //生成激活码个数，默认为100
+        $buyer = empty($_GET['buyer']) ? 'Tiny' : $_GET['buyer'];          //批量购买激活码的人
+        $owner = empty($_GET['owner']) ? 'who' : $_GET['owner'];          //激活码的用有者
+        $returnData = array();
+        $timeNow = date('Y-m-d H:i:s',time());
+        $datas = array();       //批量写入的数组数据集
+        $active = array();        //单个写入的数组数据
+        $fileDatas = array();       //要写入文件的数组数据集
+        if($owner != 'Tiny'){
+            $returnData = array('msg'=>'I do not know who you are!');
+            return_result('warning', '3000', $returnData);
+        }
+        for($i=0; $i<$count; $i++){
+            $guid = create_guid('Tiny');
+            $active = ['active_num' => $guid, 'add_time' => $timeNow, 'is_use' => 0, 'is_sale' => 1, 'owner' => $owner, 'buyer' => $buyer];
+            array_push($datas,$active);
+            $fileDatas[$i+1] = $guid;
+        }
+        try{
+            $returnCount = Db::name('active_num')->insertAll($datas);
+            if($returnCount){
+                $path = '../application/index/controller/'.date('Y-m-d',time()).'.doc';         //在linux系统下要修改controller文件夹的权限为777  chmod 777 controller/
+                $saveText = print_r($fileDatas, true);
+                if(fopen($path,'w')){
+                    file_put_contents($path, $saveText);
+                    $returnData = array('msg'=>'成功添加'.$returnCount.'条激活码，并成功生成doc文件');
+                    return_result('success', '0000', $returnData);
+                }else{
+                    $returnData = array('msg'=>'成功添加'.$returnCount.'条激活码，但是生成doc文件失败');
+                    return_result('success', '0000', $returnData);
+                }
+            }else{
+                $returnData = array('msg'=>'添加失败');
+                return_result('info', '1000', $returnData);
+            }
+        }catch(\Exception $e){
+            $returnData = array('msg'=>'服务器错误');
             return_result('error', '2000', $returnData);
         }
     }

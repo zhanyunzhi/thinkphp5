3f320c9a7156c285d3e4a83679d3718b0ed59079
zhanyunzhi <540562332@qq.com>
2017-10-31 17:11:47 +0800

    加入bootstrap，尝试修改图片预览界面

--- application/index/controller/Upload.php
+++ application/index/controller/Upload.php
@@ -11,9 +11,17 @@ class Upload {
 //      return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
         return view('./Upload/ajaxUpload',['name'=>'thinkphp']);
     }
+    public function webUploader() {
+//      return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
+        return view('./Upload/webUploader',['name'=>'thinkphp']);
+    }
+    public function uploadImageOneByOne() {
+//      return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
+        return view('./Upload/uploadImageOneByOne',['name'=>'thinkphp']);
+    }
     public function uploadOne(){
         // 获取表单上传文件 例如上传了001.jpg
-        $file = request()->file('image');
+        $file = request()->file('file');
 
         // 移动到框架应用根目录/public/uploads/ 目录下
         if($file){
@@ -25,8 +33,7 @@ class Upload {
                 // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
 //                echo $info->getSaveName();
                 // 输出 42a79759f284b767dfcb2a0197904287.jpg
-//                echo $info->getFilename();
-                return $this->AjaxReturn('yyy');
+                echo $info->getFilename();
             }else{
                 // 上传失败获取错误信息
                 echo $file->getError();
@@ -35,7 +42,7 @@ class Upload {
     }
     public function uploadMore(){
         // 获取表单上传文件
-        $files = request()->file('image');
+        $files = request()->file('file');
         foreach($files as $file){
             // 移动到框架应用根目录/public/uploads/ 目录下               限制大小还受到php.ini 中 upload_max_filesize 选项值的影响！
             $info = $file->validate(['size'=>6*1024*1024,'ext'=>'jpg,png,gif,jpeg'])->move(ROOT_PATH . 'public' . DS . 'uploads');
